4ad59f50da3d1ca9cfb66e872da9eba73d25b092
darmawanhadip <m26414143@john.petra.ac.id>
2016-09-17 21:01:17 +0700

    Update apa1.pl

--- apa1.pl
+++ apa1.pl
@@ -16,3 +16,16 @@ if (/\p{Digit}/) {      # 411 different possible characters
 if (/\p{Hex}\p{Hex}/) {
     print "The string has a
 ~
+$_ = "yabba dabba doo";
+if (/(.)\1/) { # matches 'bb' & 'oo'
+    print "It matched same character next to itself!\n";
+}
+if (/y(....) d\1/) {
+    print "It matched the same after y and d!\n";
+}
+if (/y(.)(.)\2\1/) { # matches 'abba'
+    print "It matched after the y!\n";
+}
+if (/y((.)(.)\3\2) d\1/) {
+    print "It matched!\n";
+}
